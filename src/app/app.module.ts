f5cb1ef575643151d881b68dba58cd969e0cee2b
AlexandreCoding <alexandre.alonso@softwareone.com>
2021-12-20 10:56:31 +0100

    Angular course in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// Rutas
+import { routing, appRoutingProviders } from './app.routing';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
+import { ProductosListComponent } from './components/productos-list.component';
+import { ErrorComponent } from './components/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductosListComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
